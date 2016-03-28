7ae24a2df17b7661e0169455967076c7f46d50e2
Cristian Pirvanescu <grigore_cristi92@yahoo.com>
2016-03-28 17:34:32 +0300

    test
    test

--- src/main/java/com/db/javaschool/finalproject/entity/User.java
+++ src/main/java/com/db/javaschool/finalproject/entity/User.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 @javax.persistence.Entity
 public class User implements Entity, UserDetails {
 
+	//test
 	@Id
 	@GeneratedValue
 	private Long id;
